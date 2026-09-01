3e3ec86cc8f089083e5f92cc95dd1b704156129f
robertocarloslozanoozuna-ui <robertocarloslozanoozuna@gmail.com>
2026-09-01 17:26:58 -0500

    Register admin request management API routes

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,9 @@
 import { createApp } from '../server/app.js';
 import { registerApprovalRoutes } from '../server/approvalRoutes.js';
+import { registerAdminRequestRoutes } from '../server/adminRequestRoutes.js';
 
 const app = createApp();
+registerAdminRequestRoutes(app);
 registerApprovalRoutes(app);
 
 export default app;

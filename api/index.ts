fab4eb7c0911acb097f3d1a5802f071e93933c3f
robertocarloslozanoozuna-ui <robertocarloslozanoozuna@gmail.com>
2026-09-01 17:06:57 -0500

    Restore token approval routes for serverless entrypoint

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,7 @@
 import { createApp } from '../server/app.js';
+import { registerApprovalRoutes } from '../server/approvalRoutes.js';
 
 const app = createApp();
+registerApprovalRoutes(app);
 
 export default app;

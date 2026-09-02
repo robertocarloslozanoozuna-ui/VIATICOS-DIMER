f266905c1929fb5a8862ad7a44f9c32fe47f05e3
robertocarloslozanoozuna-ui <robertocarloslozanoozuna@gmail.com>
2026-09-01 19:05:17 -0500

    Register canonical viatics request creation route

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,11 @@
 import { createApp } from '../server/app.js';
 import { registerApprovalRoutes } from '../server/approvalRoutes.js';
 import { registerAdminRequestRoutes } from '../server/adminRequestRoutes.js';
+import { registerRequestCreationRoutes } from '../server/requestCreationRoutes.js';
 
 const app = createApp();
 registerAdminRequestRoutes(app);
+registerRequestCreationRoutes(app);
 registerApprovalRoutes(app);
 
 export default app;
